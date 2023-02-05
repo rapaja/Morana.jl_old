2b7f52c6e7f77f612d6deb9812af1e8aa1c5b128
Milan Rapaić <rapaja@uns.ac.rs>
2023-02-05 08:44:27 +0100

    Basic expression types implemented

--- src/expressions.jl
+++ src/expressions.jl
@@ -4,7 +4,7 @@ struct MLiteral{T<:Number} <: MExpression
     val::T
 end
 
-MLiteral(val::T) where {T<:Number} = MLiteral{T}(val)
+Base.:(==)(l1::MLiteral{T}, l2::MLiteral{S}) where {S,T} = l1.val == l2.val
 
 struct MVariable <: MExpression
     sym::Symbol
@@ -14,6 +14,9 @@ struct MSum <: MExpression
     term1::MExpression
     term2::MExpression
 end
+
+Base.:(==)(s1::MSum, s2::MSum) = ((s1.term1 == s2.term1) && (s1.term2 == s2.term2)) ||
+                                 ((s1.term1 == s2.term2) && (s1.term2 == s2.term1))
 
 Base.:+(expr1::MExpression, expr2::MExpression) = MSum(expr1, expr2)
 Base.:+(expr::MExpression, sym::Symbol) = MSum(expr, MVariable(sym))
@@ -26,6 +29,9 @@ struct MProd <: MExpression
     factor1::MExpression
     factor2::MExpression
 end
+
+Base.:(==)(s1::MProd, s2::MProd) = ((s1.factor1 == s2.factor1) && (s1.factor2 == s2.factor2)) ||
+                                   ((s1.factor1 == s2.factor2) && (s1.factor2 == s2.factor1))
 
 Base.:-(expr::MExpression) = MProd(MLiteral(-1), expr)
 Base.:-(sym::Symbol) = MProd(MLiteral(-1), MVariable(sym))
@@ -49,6 +55,9 @@ struct MDiv <: MExpression
     den::MExpression
 end
 
+Base.:(==)(expr1::MDiv, expr2::MDiv) = (expr1.num == expr2.num) &&
+                                       (expr1.den == expr2.den)
+
 Base.:/(expr1::MExpression, expr2::MExpression) = MDiv(expr1, expr2)
 Base.:/(expr::MExpression, sym::Symbol) = MDiv(expr, MVariable(sym))
 Base.:/(sym::Symbol, expr::MExpression) = MDiv(MVariable(sym), expr)
@@ -63,61 +72,76 @@ Base.:\(sym1::Symbol, sym2::Symbol) = sym2 / sym1
 Base.:\(expr::MExpression, num::T) where {T<:Number} = num / expr
 Base.:\(num::T, expr::MExpression) where {T<:Number} = expr / num
 
+Base.://(expr1::MExpression, expr2::MExpression) = expr1 / expr2
+
+struct MPow <: MExpression
+    base::MExpression
+    exponent::MExpression
+end
+
+Base.:(==)(expr1::MPow, expr2::MPow) = (expr1.base == expr2.base) &&
+                                       (expr1.exponent == expr2.exponent)
+
+Base.:^(base::MExpression, exponent::MExpression) = MPow(base, exponent)
+Base.:^(expr::MExpression, sym::Symbol) = MPow(expr, MVariable(sym))
+Base.:^(sym::Symbol, expr::MExpression) = MPow(MVariable(sym), expr)
+Base.:^(sym1::Symbol, sym2::Symbol) = MPow(MVariable(sym1), MVariable(sym2))
+Base.:^(expr::MExpression, num::T) where {T<:Number} = MPow(expr, MLiteral(num))
+Base.:^(num::T, expr::MExpression) where {T<:Number} = MPow(MLiteral(num), expr)
+
+Base.sqrt(expr::MExpression) = MPow(expr, MLiteral(1 // 2))
+
+struct MExp <: MExpression
+    exponent::MExpression
+end
+
+Base.exp(expr::MExpression) = MExp(expr)
+
+struct MLog <: MExpression
+    argument::MExpression
+end
+
+Base.log(expr::MExpression) = MLog(expr)
+
+# struct MExpandedPolynomial <: MExpression
+#     wrt::MExpression
+#     coeffs::AbstractArray{MExpression}
+# end
+
 struct MDerivative <: MExpression
     of::MExpression
     wrt::MExpression
 end
+
+Base.:(==)(expr1::MDerivative, expr2::MDerivative) = (expr1.of == expr2.of) &&
+                                                     (expr1.wrt == expr2.wrt)
 
 struct MIntegral <: MExpression
     of::MExpression
     wrt::MExpression
 end
 
-abstract type MPowerExpression <: MExpression end
-
-struct MPower <: MPowerExpression
-    base::MExpression
-    exp::MExpression
-end
-
-struct MSquare <: MPowerExpression
-    base::MExpression
-end
-
-struct MExp <: MExpression
-    of::MExpression
-end
-
-struct MLog <: MExpression
-    of::MExpression
-end
-
-abstract type MPolynomialExpression <: MExpression end
-
-struct MExpandedPolynomial <: MPolynomialExpression
-    wrt::MExpression
-    coeffs::AbstractArray{MExpression}
-end
-
-struct MFactoredPolynomial <: MPolynomialExpression
-    wrt::MExpression
-    zeros::AbstractArray{MExpression}
-    gain::MExpression
-end
+Base.:(==)(expr1::MIntegral, expr2::MIntegral) = (expr1.of == expr2.of) &&
+                                                 (expr1.wrt == expr2.wrt)
 
 variables(::MLiteral{T}) where {T<:Number} = Set([])
 variables(expr::MVariable) = Set([expr])
 variables(expr::MSum) = union(variables(expr.term1), variables(expr.term2))
 variables(expr::MProd) = union(variables(expr.factor1), variables(expr.factor2))
 variables(expr::MDiv) = union(variables(expr.den), variables(expr.num))
+variables(expr::MPow) = union(variables(expr.base), variables(expr.exponent))
+variables(expr::MExp) = variables(expr.exponent)
+variables(expr::MLog) = variables(expr.argument)
+# variables(expr::MExpandedPolynomial) = union(variables(expr.wrt), (variables.(expr.coeffs))...)
+variables(expr::MDerivative) = union(variables(expr.of), variables(expr.wrt))
+variables(expr::MIntegral) = union(variables(expr.of), variables(expr.wrt))
 
 is_constant(expr::MExpression, var::MVariable) = !(var in variables(expr))
 is_constant(expr::MExpression, vars::AbstractArray{MVariable}) =
     all([is_constant(expr, v) for v in vars])
 
 export MExpression, MLiteral, MVariable, MSum, MProd, MDiv
+export MPowerExpression, MPow
+export MExp, MLog, MExpandedPolynomial
 export MDerivative, MIntegral
-export MPowerExpression, MPower, MSquare
-export MExp, MLog
-export MPolynomialExpression, MExpandedPolynomial, MFactoredPolynomial
 export variables, is_constant
--- test/expressions_tests.jl
+++ test/expressions_tests.jl
@@ -5,6 +5,8 @@
     s = MVariable(:s)
 
     @testset "MLiteral tests" begin
+        @test MLiteral(1 // 2) == MLiteral(1 / 2)
+        @test MLiteral(1 // 2) == MLiteral(0.5)
         @test variables(lit1) == Set([])
         @test is_constant(lit1, t)
         @test is_constant(lit1, s)
@@ -28,6 +30,9 @@
         @test (t + lit1) == (t + 1)
         @test (lit1 + t) == (1 + t)
         @test variables(1 + t) == Set([t])
+        @test s + t == t + s
+        @test s + t != 1 + t
+        @test s + t != t
     end
 
     @testset "Multiplication tests" begin
@@ -41,6 +46,9 @@
         @test (t * lit1) == (t * 1)
         @test (lit1 * t) == (1 * t)
         @test variables(1 * t) == Set([t])
+        @test s * t == t * s
+        @test s * t != 1 * t
+        @test s * t != t
     end
 
     @testset "Subtraction tests" begin
@@ -69,6 +77,7 @@
         @test (t / lit1) == (t / 1)
         @test (lit1 / t) == (1 / t)
         @test variables(1 / t) == Set([t])
+        @test (t / s) == (t // s)
     end
 
     @testset "Left division tests" begin
@@ -77,6 +86,36 @@
         @test (t \ s) == (:s / t)
         @test (t \ s) == (:s / :t)
         @test (t \ 1) == (1 / t)
+    end
+
+    @testset "Power expression tests" begin
+        @test (t^s) == MPow(t, s)
+        @test variables(t^s) == Set([t, s])
+        @test !is_constant(t^s, t)
+        @test !is_constant(t^s, s)
+        @test t^0.5 == sqrt(t)
+    end
+
+    @testset "Exponential expressions tests" begin
+        @test exp(t) == MExp(t)
+        @test !is_constant(exp(s + t), t)
+        @test !is_constant(exp(s * t), s)
+        @test variables(exp(s + t)) == Set([s, t])
+    end
+
+    @testset "Exponential expressions tests" begin
+        @test log(t) == MLog(t)
+        @test !is_constant(log(s + t), t)
+        @test !is_constant(log(s * t), s)
+        @test variables(log(s + t)) == Set([s, t])
+    end
+
+    @testset "Misc tests" begin
+        @test (t^0.5 / 2) == (sqrt(t) / 2)
+        @test variables(MDerivative(s, t)) == Set([s, t])
+        @test variables(MIntegral(s, t)) == Set([s, t])
+        # @test variables(MExpandedPolynomial(t, [MLiteral(2), MLiteral(3)])) == Set([t])
+        # @test variables(MExpandedPolynomial(t, [s, 2 * s])) == Set([t, s])
     end
 
 end
