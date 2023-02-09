60adc91ab338a7f7d22638277cfb42aeeea9e51b
Milan Rapaić <rapaja@uns.ac.rs>
2023-02-09 09:09:07 +0100

    MultiPocket basic functionality

--- src/utils/_multi_pocket.jl
+++ src/utils/_multi_pocket.jl
@@ -1,41 +1,19 @@
-#TODO: prevent adding zero as cardinality to MultiPocket
 struct MultiPocket{T,N}
     parent::MultiPocket{T,N}
     item::T
     card::N
 
     MultiPocket{T,N}() where {T,N} = new()
-    MultiPocket{T,N}(item, card) where {T,N} = (empty = new(); new(empty, item, card))
-    MultiPocket{T,N}(parent::MultiPocket{T,N}, item, card) where {T,N} = new(parent, item, card)
-end
+    MultiPocket{T,N}(item, card) where {T,N} =
+        (empty = new(); iszero(card) ? empty : new(empty, item, card))
 
-function cardinality_of(bag::MultiPocket{T,N}, item) where {T,N}
-    if !isdefined(bag, :parent)
-        zero(N)
-    elseif bag.item == item
-        bag.card
-    else
-        cardinality_of(bag.parent, item)
-    end
-end
-
-# TODO: Handle the zero cardinality case
-function modified_with(bag::MultiPocket{T,N}, item, card, modifier) where {T,N}
-    if !isdefined(bag, :parent)
-        MultiPocket{T,N}(item, card)
-    elseif bag.item == item
-        new_card = modifier(bag.card, card)
-        if new_card == bag.card
-            bag
+    function MultiPocket{T,N}(parent::MultiPocket{T,N}, item, card) where {T,N}
+        if iszero(card)
+            parent
+        elseif item ∈ parent
+            throw(KeyError("Cannot instantiate a bag with a duplicate key."))
         else
-            MultiPocket{T,N}(bag.parent, item, new_card)
-        end
-    else
-        mod_parent = modified_with(bag.parent, item, card, modifier)
-        if mod_parent == parent
-            bag
-        else
-            MultiPocket{T,N}(mod_parent, bag.item, bag.card)
+            new(parent, item, card)
         end
     end
 end
@@ -45,5 +23,34 @@ MultiPocket(parent::MultiPocket{T,N}, pair::Pair) where {T,N} = MultiPocket{T,N}
 MultiPocket(parent::MultiPocket{T,N}, pair::Pair, others::Pair...) where {T,N} = MultiPocket(MultiPocket(parent, pair), others...)
 MultiPocket(pair::Pair{T,N}, others::Pair...) where {T,N} = MultiPocket(MultiPocket(pair), others...)
 
-export MultiPocket
-export cardinality_of, modified_with
+Base.isempty(bag::MultiPocket) = !isdefined(bag, :parent)
+Base.length(bag::MultiPocket) = isempty(bag) ? 0 : 1 + length(bag.parent)
+Base.in(item, bag::MultiPocket) = get(bag, item) != 0
+Base.get(bag::MultiPocket{T,N}, item) where {T,N} = isempty(bag) ? zero(N) : (bag.item == item ? bag.card : get(bag.parent, item))
+Base.haskey(bag::MultiPocket, item) = !isempty(bag) && (bag.item == item || haskey(bag.parent, item))
+
+function insert(bag::MultiPocket{T,N}, item, card, modifier) where {T,N}
+    if iszero(card)
+        bag
+    elseif !isdefined(bag, :parent)
+        MultiPocket{T,N}(item, card)
+    elseif bag.item == item
+        new_card = modifier(bag.card, card)
+        if new_card == bag.card
+            bag
+        elseif iszero(new_card)
+            bag.parent
+        else
+            MultiPocket{T,N}(bag.parent, item, new_card)
+        end
+    else
+        mod_parent = insert(bag.parent, item, card, modifier)
+        if mod_parent == parent
+            bag
+        else
+            MultiPocket{T,N}(mod_parent, bag.item, bag.card)
+        end
+    end
+end
+
+export MultiPocket, insert
--- test/test_utils/test_multi_pocket.jl
+++ test/test_utils/test_multi_pocket.jl
@@ -1,26 +1,51 @@
 @testset "MultiPocket tests" begin
 
+    e = MultiPocket{Symbol,Int64}()
     a = MultiPocket(:x => 1)
     b = MultiPocket(:x => 1, :y => 2)
     c = MultiPocket(b, :z => 3)
 
-    @testset "Test cardinality_of" begin
-        @test cardinality_of(a, :x) == 1
-        @test cardinality_of(b, :y) == 2
-        @test cardinality_of(c, :x) == 1
-        @test cardinality_of(c, :z) == 3
-        @test cardinality_of(c, :p) == 0
+    @testset "Initialization tests" begin
+        @test_throws KeyError MultiPocket(a, :x => 2)
+        @test_throws KeyError MultiPocket(c, :x => 2)
     end
 
-    @testset "Test modified_with" begin
+    @testset "Basic utilities tests" begin
+        @test isempty(e)
+        @test !isempty(a) && !isempty(b) && !isempty(c)
+        @test length(e) == 0 && length(a) == 1 && length(b) == 2 && length(c) == 3
+        @test :x ∈ a && :x ∈ b && :x ∈ c
+        @test :z ∉ a && :z ∉ b && :z ∈ c
+    end
+
+    @testset "Test get" begin
+        @test get(a, :x) == 1
+        @test get(b, :y) == 2
+        @test get(c, :x) == 1
+        @test get(c, :z) == 3
+        @test get(c, :p) == 0
+    end
+
+    @testset "Test in & haskey" begin
+        isin(bag, item) = (item ∈ bag) && haskey(bag, item)
+        @test isin(a, :x)
+        @test isin(b, :y)
+        @test isin(c, :x)
+        @test isin(c, :z)
+        @test !isin(a, :y)
+        @test !isin(c, :p)
+    end
+
+    @testset "Test insert" begin
         modifier = (old_card, new_card) -> new_card
-        @test cardinality_of(modified_with(a, :x, 2, modifier), :x) == 2
-        @test modified_with(a, :x, 1, modifier) === a
-        @test cardinality_of(modified_with(c, :x, 2, modifier), :x) == 2
-        @test modified_with(c, :x, 1, modifier) === c
-        @test cardinality_of(modified_with(c, :z, 4, modifier), :z) == 4
-        @test modified_with(c, :z, 3, modifier) === c
-        @test cardinality_of(modified_with(c, :p, 4, modifier), :p) == 4
+        @test get(insert(a, :x, 2, modifier), :x) == 2
+        @test insert(a, :x, 1, modifier) === a
+        @test get(insert(c, :x, 2, modifier), :x) == 2
+        @test insert(c, :x, 1, modifier) === c
+        @test get(insert(c, :z, 4, modifier), :z) == 4
+        @test insert(c, :z, 3, modifier) === c
+        @test get(insert(c, :p, 4, modifier), :p) == 4
+        @test get(insert(c, :a, 0, modifier), :a) == 0
     end
 
 end
