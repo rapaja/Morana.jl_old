e3bc16a1fa09e5e43075bccf685ef2c28c856ee1
Milan Rapaić <repmilan@gmail.com>
2023-08-09 07:20:46 +0200

    logger_loop

--- src/discrete/_control_loop.jl
+++ src/discrete/_control_loop.jl
@@ -1,18 +1,19 @@
 ones_like(x) = ones(eltype(x), size(x))
 
+# TODO: Add time to all the functions. Disturbance could be added internally
+# TODO: Add generic logging.
 function control_loop(plant::DiscreteSystem, ctrl::DiscreteSystem, x0::Vector{T}, z0::Vector{T}, N::Number)
     xx = NaN * ones(eltype(x0), N, states_no(plant))
     yy = NaN * ones(eltype(x0), N, outputs_no(plant))
     zz = NaN * ones(eltype(z0), N, states_no(ctrl))
     uu = NaN * ones(eltype(x0), N, outputs_no(ctrl))
-    dd = NaN * ones_like(uu)
 
     xx[1, :] = x0
     zz[1, :] = z0
     for i in 1:(N-1)
         yy[i, :] = output(plant, xx[i, :])
         uu[i, :] = output(ctrl, zz[i, :], yy[i, :])
-        xx[i+1, :] = next_state(plant, xx[i+1, :], uu[i, :] + dd[i, :])
+        xx[i+1, :] = next_state(plant, xx[i+1, :], uu[i, :])
         zz[i+1, :] = next_state(ctrl, zz[i, :], yy[i, :])
     end
 end
---
+++ src/utils/_logger.jl
@@ -0,0 +1,3 @@
+struct Logger
+    # logs::
+end
